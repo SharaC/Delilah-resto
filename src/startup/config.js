8688832a878131a71e0f3c2dc6ee2b81a5c0c167
SharaC <sarita.sh@hotmail.com>
2020-11-27 17:17:30 -0500

    Update config.js

--- src/startup/config.js
+++ src/startup/config.js
@@ -1,9 +1,9 @@
 const config = {
     appPort: 3030,
-    HOST: 'localhost',
-    DATABASE: 'delilah_resto',
-    USER_DATABASE: 'root',
-    PASSWORD_DATABASE: 'querty123',
+    HOST_DATABASE: 'localhost',
+    NAME_DATABASE: 'delilah_resto',
+    USER_DATABASE: 'usuario_database',
+    PASSWORD_DATABASE: 'password_usuario',
     apiVersion: "/v1",
     SECRET: 'DELILAH-RESTO'
 }
